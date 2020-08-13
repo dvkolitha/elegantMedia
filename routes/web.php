edc0af1187ce94db2f9e44df1914d413e6b276de
dvkolitha <dvkolitha@gmail.com>
2020-08-13 19:48:14 +0530

    Update 1\

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,6 @@
 */
 
 
-
-/*route for test purpose*/
-Route::get('/createTestUser',function () {
-  $user = new App\User;
-  $user->name = 'kolitha';
-  $user->email = 'dvkolitha@gmail.com';
-  $user->is_support_agent = 1 ;
-  $user->password = bcrypt('kolitha');
-  $user->save();      
- echo 'User was created';
-}); 
-
 /*
 Guest User Required Routes
 */
